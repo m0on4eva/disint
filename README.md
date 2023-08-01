8c97c0aead60066341a6bc06d97e1a93e9698838
L0tsow <85215760+L0tsow@users.noreply.github.com>
2023-08-01 20:02:05 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# DisInt
+Use of DisInt ?
+
+    DisInt is a homemade tool for Osint 
+    You can by just print the discord's id  found a lot of information about an account, like his avatar, banner link to make a search by image.
+    You also got more information. 
+
+What mean DisInt ?
+
+    DisInt is the contraction of "Discord"and "Osint"
+
+Usage & setup
+
+    on line 7, to the value "token".
+    in the "" after the =, you must enter the token of a discord account that will be able to make requests.
+    I advise you to create a useless account and use its token to make requests with the API
+
+
+
+<img src="https://cdn.discordapp.com/attachments/1129019725146505339/1135995292928446596/pfp5.png" width=120 style="margin-top: 10px; filter: drop-shadow(0 0 0.75rem white);">
+<a style="position : absolute; margin-top : 40px; margin-left : 20px; font-size : 24px "> Made by L0tsow</a>
---
+++ main.py
@@ -0,0 +1,66 @@
+from colorama import Fore
+import json
+import requests
+import os
+
+
+token = "token value"
+
+
+def lotsow():
+    os.system('CLS')
+    os.system('clear')
+    print(Fore.LIGHTMAGENTA_EX + """
+ ____  _     ___       _   
+|  _ \(_)___|_ _|_ __ | |_ 
+| | | | / __|| || '_ \| __|
+| |_| | \__ \| || | | | |_ 
+|____/|_|___/___|_| |_|\__|   
+    """)
+    print(Fore.LIGHTRED_EX + "Github : 0xL0tsow ")
+    print(Fore.LIGHTBLUE_EX + "         Twitter : 0xL0tsow")
+    print(Fore.LIGHTGREEN_EX + "                    Telegram : L0tsow")
+    print(Fore.LIGHTYELLOW_EX + "                               Discord : 0xL0tsow")
+    print(Fore.WHITE + "-" * 50)
+
+lotsow()
+
+id_ds = input("Enter victim's discord id : ")
+
+url = "http://canary.discord.com/api/v9/users/" + id_ds
+
+headers = {
+    "Authorization": token,
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    user_id = data['id']
+
+    user = requests.get(f"http://canary.discord.com/api/v9/users/{user_id}", headers=headers).text
+
+    lotsow()
+
+    print("Account Data:")
+    print("-" * 50)
+       
+    user_data = json.loads(user)
+    for key, value in user_data.items():
+        print(f"{key} : {value} \n")
+    print(f"avatar link: https://cdn.discordapp.com/avatars/{user_id}/{user_data['avatar']} \n")
+
+    print(f"banner link: https://cdn.discordapp.com/banners/{user_id}/{user_data['banner']}.gif?size=480 \n")
+    print("-" * 50)
+
+    with open(f"output/{user_data['username']}" +'.txt', "a+") as discord_output:
+        discord_output.write(str(user_data))
+        discord_output.write("\n\n")
+        discord_output.write("This tool is a preview of the big tool who is coming soon :) ENJOY ! \n")
+        discord_output.write("        Follow me on : \n")
+        discord_output.write("Twitter - https://twitter.com/0xl0tsow \n")
+        discord_output.write("Github - https://github.com/0xL0tsow \n")                                      
+        discord_output.write("Telegram - https://t.me/L0tsow \n")        
+        discord_output.close()
+exit()
